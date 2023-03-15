83f730197dabb9b117d9a91debf411f4220891d0
Einar Engstrom <meengst@gmail.com>
2023-03-15 17:01:39 -0400

    upgraded elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blog.MixProject do
     [
       app: :blog,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
