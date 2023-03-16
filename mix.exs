120adf62cdaeb25c9ff4376bb472406d1b8120c5
Marko Vukovic <mvkvc.git@gmail.com>
2023-03-15 22:38:29 -0400

    Add tailwind to assets.deploy

--- mix.exs
+++ mix.exs
@@ -85,7 +85,8 @@ defmodule Blog.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      docs: ["docs --formatter html"]
     ]
   end
 end
